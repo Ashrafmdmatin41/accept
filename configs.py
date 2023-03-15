c599439c54df37b7ebf13f1a1b4f4ee8d18d825f
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-03-15 16:56:15 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -6,12 +6,12 @@ from os import environ as evn
 id_pattern = re.compile(r'^.\d+$')
 
 class Config:
-    API_ID = int(getenv("API_ID", "0112234"))
-    API_HASH = getenv("API_HASH", "abcdefg")
-    BOT_TOKEN = getenv("BOT_TOKEN", "1234567891:AdDfgFRFVVfDEhdhyjjvjjftSEW")
+    API_ID = int(getenv("API_ID", "26756378"))
+    API_HASH = getenv("API_HASH", "28f615585ac59b2ec39e5fe828de21a0")
+    BOT_TOKEN = getenv("BOT_TOKEN", "6059695462:AAHHGQk0mQj-Q06Yt7_8yoiBejI-HwCN90k")
     FSUB = getenv("FSUB", "beta_botz")
     CHID = int(getenv("CHID", "-1001858865209"))
     SUDO = [int(admin) if id_pattern.search(admin) else admin for admin in evn.get('SUDO', '5558249587').split()]
-    MONGO_URI = getenv("MONGO_URI", "")
+    MONGO_URI = getenv("mongodb+srv://ownbot:earpr1500m@cluster0.5mejmj5.mongodb.net/?retryWrites=true&w=majority", "")
     
 cfg = Config()
