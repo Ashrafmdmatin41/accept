cd6ad8447c4fbe89c1a4072dcf28c3f1ec92f552
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-03-17 20:37:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/rohitjangra11/File.git /File
+  git clone https://github.com/rohitjangra11/accept.git /Accept
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /File
