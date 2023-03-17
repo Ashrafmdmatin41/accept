09fe122a522cdfa648b8d13169cdf50cae887ce2
rohitjangra11 <126782133+rohitjangra11@users.noreply.github.com>
2023-03-17 20:55:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,8 @@
-if [ -z $UPSTREAM_REPO ]
-then
+
   echo "Cloning main Repository"
   git clone https://github.com/rohitjangra11/accept.git /Accept
-else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /accept
-fi
-cd /File
+cd /accept
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙀𝙡𝙨𝙖....🧞‍♂️"
 python3 bot.py
